40fa4cfd0e3a10c43783c3b9448074825437fed1
Namigodara <namigodara@gmail.com>
2020-06-26 14:03:31 -0700

    added answer for Q5

---
+++ Task1_Q5.py
@@ -0,0 +1,11 @@
+#take input from user 
+print("Please enter first number between 1-10" )
+x = int(input())
+print("Please enter second number between 1 -10")
+y = int(input())
+z = 30
+Result = x+y+z 
+print("sum of all three variable is = ", Result)
+
+
+
