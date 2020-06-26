c87aeb3f72834ddaa604897278a6d05768d05f93
Namigodara <namigodara@gmail.com>
2020-06-26 12:26:32 -0700

    added Q2 in separate file

---
+++ Task1_Q2.py
@@ -0,0 +1,9 @@
+# Create a complex variable type and swap it with another variable type Int
+a,b = 1+2j, 10
+print("a= ", a)
+print("b= ", b)
+ # swap the values
+a,b = b,a
+print("Values after swapping")
+print("a= ", a)
+print("b= ", b)
--- Task_1.py
+++ Task_1.py
@@ -4,12 +4,3 @@ print("x:", x)
 print("y:", y)
 print("z:", z)
 
-# Create a complex variable type and swap it with another variable type Int
-a,b = 1+2j, 10
-print("a= ", a)
-print("b= ", b)
- # swap the values
-a,b = b,a
-print("Values after swapping")
-print("a= ", a)
-print("b= ", b)
