ab2be970298bb8486f89def17975fffdf2f63f00
Namigodara <namigodara@gmail.com>
2020-06-26 12:22:35 -0700

    added answer for Q2

--- Task_1.py
+++ Task_1.py
@@ -3,3 +3,13 @@ x,y,z = 30, 10.5, "apple"
 print("x:", x)
 print("y:", y)
 print("z:", z)
+
+# Create a complex variable type and swap it with another variable type Int
+a,b = 1+2j, 10
+print("a= ", a)
+print("b= ", b)
+ # swap the values
+a,b = b,a
+print("Values after swapping")
+print("a= ", a)
+print("b= ", b)
