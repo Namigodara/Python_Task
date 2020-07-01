e540d14d7b89b6a152b52f11f658cf218e8cfc8c
Namigodara <namigodara@gmail.com>
2020-07-01 11:19:51 -0700

    deleted file Task1_Q2.py

--- Task1_Q2.py
+++
@@ -1,9 +0,0 @@
-# Create a complex variable type and swap it with another variable type Int
-a,b = 1+2j, 10
-print("a= ", a)
-print("b= ", b)
- # swap the values
-a,b = b,a
-print("Values after swapping")
-print("a= ", a)
-print("b= ", b)
