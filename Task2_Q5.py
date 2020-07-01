8eaae36ebf44397cedf19d129e6236e5ce5618e1
Namigodara <namigodara@gmail.com>
2020-07-01 12:58:35 -0700

    removed Task2 files

--- Task2_Q5.py
+++
@@ -1,9 +0,0 @@
-# write a program to find numbers in range 2000 to 3200
-# number should be divisable by 7 but shouldn't be multiple of 5.
-
-print("Find number between 2000 to 3200, divisable by 7 but not by 5")
-for number in range(2000, 3200):
-    if (number % 7 == 0):
-        if(number % 5 != 0):
-            print(number)
-print ("End of the program")
