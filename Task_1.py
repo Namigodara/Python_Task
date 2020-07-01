9032de66f3d6577a86b0ca37c03e532c6c4ef251
Namigodara <namigodara@gmail.com>
2020-07-01 11:15:20 -0700

    added code for Q2 in same file

--- Task_1.py
+++ Task_1.py
@@ -1,6 +1,22 @@
+# Question 1 :
 # create and assign value to three variable in single line.
+# All three values should be different data type.
+print("Code for Task_1 Question_1 starts here")
 x,y,z = 30, 10.5, "apple"
 print("x:", x)
 print("y:", y)
 print("z:", z)
+
+
+# Question 2 :
+# Create a complex variable type and swap it with another variable type Int
+print("Code for Task_1 Question_2 starts here")
+a,b = 1+2j, 10
+print("a= ", a)
+print("b= ", b)
+ # swap the values
+a,b = b,a
+print("Values after swapping")
+print("a= ", a)
+print("b= ", b)
 
