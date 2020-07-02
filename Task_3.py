f258f58cb036f779412c80774ba31753745b1236
Namigodara <namigodara@gmail.com>
2020-07-02 15:26:49 -0700

    added file for Task 3

---
+++ Task_3.py
@@ -0,0 +1,8 @@
+# Question 1 :
+# Create a list of 10 element.
+# it should contain four data type : int,float, complex and float.
+
+print ("Code for Task_3 Q_1 starts here")
+
+mix_list = [1,2,3,4,"Nami","made","this", 18.5, -1.6, 2+4j, -1+5j]
+print(mix_list
