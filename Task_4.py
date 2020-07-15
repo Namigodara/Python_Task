cf6436488ce3cdbb27b5f2d3d9668019cbd8e491
Namigodara <namigodara@gmail.com>
2020-07-14 17:54:12 -0700

    addede code for task4 Q6,Q7,Q8

--- Task_4.py
+++ Task_4.py
@@ -106,3 +106,70 @@ for i in range(count):
     
 print ("Output seq is: ")
 print (*l, sep = "\n")
+
+
+
+# Question 6:
+#  Write a function that can take two integral as string
+# compute their sum and print in console
+
+print ("code for task4 Q6 starts here:")
+
+print("Enter the first string of number:")
+str1 = input()
+print("Enter the second string of number:")
+str2 = input()
+
+def sum_str(x1,x2):
+    int1 = int(x1)
+    int2 = int(x2)
+    sum = int1 +int2
+    return sum
+
+res = sum_str(str1,str2)
+print ("Sum of two numbers is :", res)
+
+
+
+# Question 7:
+# Write a function which accepts two strings as input
+# prints the string with maximum length
+# if both are same length then it prints both of them
+
+print("code for task4 Q7 starts here:")
+
+print ("enter the firts string:")
+str1 = input()
+print ("enter the second string:")
+str2 = input()
+
+def max_len(s1,s2):
+    if (len(s1)==len(s2)):
+        print (s1)
+        print (s2)
+    elif (len(s1) > len(s2)):
+        print (s1)
+    else:
+        print (s2)
+
+print ("output:")
+max_len(str1,str2)
+
+
+
+# Question 8:
+# Define a function to generate a tuple
+# values of tuple are square of number 1 to 20
+
+print ("Code for task4 Q8 starts here:")
+
+def square(n):
+    l = []
+    for i in range(1,n):
+        l.append(i**2)
+   
+    x_tuple = tuple(l)
+    return (x_tuple)
+
+x_out = square(20)
+print(x_out)
